f241e7272b20fd527b6a6f413bc06ea2a3e264b0
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-05-11 14:17:10 -0700

    Odo Peter Ebere

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
 
-if len(argv) == 1:
-    print("0 arguments.")
-elif len(argv) > 1:
-    for i, args in enumerate(argv):
-        if(i != 0):
-            print(f"{i}: {argv[i]}")
+if __name__ == "__main_":
+    from sys import argv
+
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) > 1:
+        for i, args in enumerate(argv):
+            if(i != 0):
+                print(f"{i}: {argv[i]}")
