24f36e1d5423b9624e82991836a30cb45a07ccc4
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-05-15 23:20:55 -0700

    Odo Peter Ebere

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,6 +4,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if (len(tuple_a)) == 0 and len(tuple_b) == 2:
         tuple_total = ((tuple_b[0]), (tuple_b[1]))
         return tuple_total
+    elif (len(tuple_b)) == 1 and len(tuple_a) == 1:
+        tuple_total = ((tuple_a[0]), (tuple_b[0]))
+        return tuple_total
+    if (len(tuple_a)) == 0 and len(tuple_b) == 0:
+        return tuple_total
     elif (len(tuple_b)) == 0 and len(tuple_a) == 2:
         tuple_total = ((tuple_a[0]), (tuple_a[1]))
         return tuple_total
@@ -14,11 +19,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_total = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + 0))
         return tuple_total
     if (len(tuple_a)) == 0 and len(tuple_b) == 2:
-         tuple_total = ((tuple_b[0]), (tuple_b[1]))
-         return tuple_total
+        tuple_total = ((tuple_b[0]), (tuple_b[1]))
+        return tuple_total
     elif (len(tuple_b)) == 0 and len(tuple_a) == 2:
-          tuple_total = ((tuple_a[0]), (tuple_a[1]))
-          return tuple_total
+        tuple_total = ((tuple_a[0]), (tuple_a[1]))
+        return tuple_total
     if (len(tuple_a)) > 2 or (len(tuple_b)) > 2:
         tuple_total = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
         return tuple_total
