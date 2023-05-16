91368686d40e5d811d82c1a1996afbb7cb9a816a
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-05-16 00:12:24 -0700

    Odo Peter Ebere

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if len(my_list) == 0 and idx == 0:
-        return None
+    if len(my_list) == 0:
+        return []
     elif (idx < 0 or idx > (len(my_list) - 1)):
         return my_list
     else:
