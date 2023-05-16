d0251f6af787d0c4d502d3a6008ee40e43015221
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-05-15 23:23:24 -0700

    Odo Peter Ebere

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_total = ()
-    if (len(tuple_a)) == 0 and len(tuple_b) == 2:
+    if (len(tuple_a)) == 0 and len(tuple_b) == 0:
         tuple_total = ((tuple_b[0]), (tuple_b[1]))
         return tuple_total
     elif (len(tuple_b)) == 1 and len(tuple_a) == 1:
         tuple_total = ((tuple_a[0]), (tuple_b[0]))
         return tuple_total
-    if (len(tuple_a)) == 0 and len(tuple_b) == 0:
+    if (len(tuple_a)) == 0 and len(tuple_b) == 2:
+        tuple_total = ((tuple_b[0]), (tuple_b[1]))
         return tuple_total
     elif (len(tuple_b)) == 0 and len(tuple_a) == 2:
         tuple_total = ((tuple_a[0]), (tuple_a[1]))
