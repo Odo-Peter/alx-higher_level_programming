f4ccc20e7f8d6c66cc50b45da01fd578f0e21db5
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-05-15 23:13:42 -0700

    Odo Peter Ebere

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,7 @@ def print_reversed_list_integer(my_list=[]):
     list_len = len(my_list) - 1
     if (my_list is None):
         return ()
-    while list_len > -1:
-        print("{:d}".format(my_list[list_len]))
-        list_len -= 1
+    else:
+        while list_len > -1:
+            print("{:d}".format(my_list[list_len]))
+            list_len -= 1
