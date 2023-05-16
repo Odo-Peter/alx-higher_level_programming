42c9a9012725ebbc23aeba3ebd7ed948f523d7b6
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-05-15 23:51:59 -0700

    Odo Peter Ebere

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return ()
+        return None
     else:
-        temp = 0
+        temp = my_list[0]
         for i in my_list:
             if i > temp:
                 temp = i
