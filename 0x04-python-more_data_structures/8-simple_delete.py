e73951675c2452fa5c660d788cb155d43813c635
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-05-18 01:58:28 -0700

    Odo Peter Ebere

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key is None:
+    if len(a_dictionary) != 0:
+        if key is None:
+            return (a_dictionary)
+        elif key not in a_dictionary:
+            return (a_dictionary)
+        else:
+            del a_dictionary[key]
         return (a_dictionary)
-    else:
-        del a_dictionary[key]
-    return (a_dictionary)
