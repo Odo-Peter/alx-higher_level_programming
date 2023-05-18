66c15b2414a490cad17b68bbf8a331f91af4def7
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-05-18 00:05:07 -0700

    Odo Peter Ebere

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return
+    else:
+        new_list = my_list.copy()
+        idx = my_list.index(search)
+        if idx is None:
+            return
+        else:
+            new_list[idx] = replace
+        return new_list
