ebfccf0fd2f0ab808749ae62a5f2b3a3b45a2622
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-05-18 00:56:20 -0700

    Odo Peter Ebere

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    if my_list is None:
+        return ()
+    else:
+        new_list = []
+        for i in my_list:
+            if i not in new_list:
+                new_list.append(i)
+        sum_of_val = 0
+        print(new_list)
+        for x in new_list:
+            sum_of_val += x
+        return (sum_of_val)
