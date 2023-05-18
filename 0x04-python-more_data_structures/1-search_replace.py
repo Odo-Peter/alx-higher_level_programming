c5576d644ce668c296c275e55242629f29dcfd14
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-05-18 00:14:48 -0700

    Odo Peter Ebere

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
     if len(my_list) == 0 or my_list is None:
-        return None
-    elif search > len(my_list):
-        return None
+        return ()
     else:
-        new_list = my_list.copy()
-        idx = my_list.index(search)
-        if idx is None:
-            return
-        else:
-            new_list[idx] = replace
-        return new_list
+        idx = new_list_list.index(search)
+        new_list[idx] = replace
+    return new_list
