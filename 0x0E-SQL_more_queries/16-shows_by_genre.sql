134c1674d0d8d111073a3d9176d2fcb18d48966e
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-07-20 22:00:23 -0700

    Odo Peter Ebere - SQL_more_queries

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -6,4 +6,4 @@ FROM tv_shows
     	 ON tv_show_genres.show_id = tv_shows.i
     LEFT JOIN tv_genres
     	 ON tv_genres.id = tv_show_genres.genre_id
-    ORDER BY tv_shows.title, tv_genres.name;
+ORDER BY tv_shows.title, tv_genres.name;
