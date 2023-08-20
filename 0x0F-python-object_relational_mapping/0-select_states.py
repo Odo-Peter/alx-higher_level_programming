ab9f7554876ed5b6fb3d8bec55f8fbeea7bc90de
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-08-20 10:07:54 -0700

    Odo Peter Ebere - python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,15 +6,14 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-        db = MySQLdb.connect(
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306,
-            host='localhost')
-        cursor = db.cursor()
-        cursor.execute('SELECT * FROM states ORDER BY states.id ASC;')
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306,
+                         host='localhost')
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states ORDER BY states.id ASC;')
 
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,12 +7,11 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306,
-        host='localhost')
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306,
+                         host='localhost')
 
     cursor = db.cursor()
     cursor.execute("SELECT * \
