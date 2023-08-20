87de022e980a7dd2c010f765f1ceff1e6548aaeb
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-08-20 10:57:45 -0700

    Odo Peter Ebere - python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Changes and update the name of a State object
+'''
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+    InstanceSession = sessionmaker(bind=engine)
+    session = InstanceSession()
+
+    states = session.query(State).filter(State.id == 2)
+    for state_el in states:
+        state_el.name = 'New Mexico'
+
+    session.commit()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+script that takes in the name of a state as an argument,
+queries and lists all cities in state
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306,
+                         host='localhost')
+
+    cursor = db.cursor()
+    cursor.execute(
+        'SELECT cities.name FROM cities\
+        INNER JOIN states ON cities.state_id = states.id\
+        WHERE states.name = %s \
+        ORDER BY cities.id ASC', (sys.argv[4], ))
+
+    cities = cursor.fetchall()
+
+
+    indx = 0
+    for city in cities:
+        if indx != 0:
+            print(", ", end="")
+        print("%s" % city, end="")
+        indx += 1
+    print("")
+
+    cursor.close()
+    db.close()
